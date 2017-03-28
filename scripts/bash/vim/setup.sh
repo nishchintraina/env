bb93ea5d7b95125c0a43cbef24b0f3b667094e25
Nishchint Raina <nishchint@protonmail.ch>
2017-03-28 01:06:58 -0700

    Add vim plugins: pathogen and vim-airline-themes

--- scripts/bash/vim/setup.sh
+++ scripts/bash/vim/setup.sh
@@ -4,7 +4,14 @@ THISDIR=$(dirname "$0")
 cp ${THISDIR}/.vimrc ~
 
 mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/autoload
 
 if [ ! -d ~/.vim/bundle/vim-airline ]; then
   git clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/vim-airline
 fi
+
+if [ ! -d ~/.vim/bundle/vim-airline-themes ]; then
+  git clone https://github.com/vim-airline/vim-airline-themes.git ~/.vim/bundle/vim-airline-themes
+fi
+
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
